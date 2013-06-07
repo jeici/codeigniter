0413d91f7e23f14c7f120f1dc03ba8268de58232
JEYSSON <jeyssonfcb7@hotmail.com>
2013-06-07 02:20:16 -0600

    cambie la base url en el config.php por http://localhost:8081/codeigniter/index.php si ocupan el 8080 solo borrar el 8181 del url y no dar commit a este cambio

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/codeignitertemp/';
+$config['base_url']	= 'http://localhost:8081/codeigniter/';
 
 /*
 |--------------------------------------------------------------------------
